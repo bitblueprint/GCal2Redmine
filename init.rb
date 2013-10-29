5bc9f3119798a5318d755976ccb86c97990a84e8
Markus Færevaag <mf@bitblueprint.com>
2013-10-29 12:56:50 +0100

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :gcal2redmine do
+  name 'GCal2Redmine plugin'
+  author 'BIT BLUEPRINT'
+  description 'Google Calendar events to Redmine time entries'
+  version '0.0.1'
+  url 'https://github.com/bitblueprint/GCal2Redmine'
+  author_url 'http://bitblueprint.com'
+end
